d12cffcc09294bdb3113a91c35ed0e12542275bc
Kyla Ester <esterkyla@outlook.com>
2023-05-10 18:38:10 -0400

    finished lesson page 2 styling

---
+++ src/Pages/Lesson3/Lesson3.js
@@ -0,0 +1,23 @@
+import "./Lesson3.scss";
+// import Nav from "../../Components/Nav/Nav";
+// // import { useEffect, useState } from "react";
+// // import axios from "axios";
+// // import { Link } from "react-router-dom";
+// // import html from "../../Assets/html (2).png";
+// // import arrow from "../../Assets/down-arrow.png";
+// // import back from "../../Assets/angle-left.svg";
+// import TwoLesson from "../../Components/TwoLesson/TwoLesson";
+// import { Link } from "react-router-dom";
+// import Background from "../../Components/Background/Background";
+// import back from "../../Assets/angle-left.svg";
+// import points from "../../Assets/golde_icon.svg";
+// import bear from "../../Assets/Group.svg";
+// import leaf1 from "../../Assets/Vector.svg";
+// import plant from "../../Assets/plant.png";
+// import { Link } from "react-router-dom";
+
+function Lesson3() {
+  return <section className="background"></section>;
+}
+
+export default Lesson3;
